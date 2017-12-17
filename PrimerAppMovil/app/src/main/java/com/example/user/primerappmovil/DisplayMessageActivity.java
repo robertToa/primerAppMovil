b30fbc0e61a4e9d28458a9639e790806050a3c86
DESKTOP-T42SNH4\USER <robrt_pm@hotmail.com>
2017-12-17 16:42:52 -0500

    En la practica 03, creamos una nueva interfaz la cual imprime o muestra el texto ingresado en la interfaz principal cuando damos clic en el boton Send, Y se creao la navegacion que nos permite regresar de la segunda ventana a la ventana principal

--- PrimerAppMovil/app/src/main/java/com/example/user/primerappmovil/DisplayMessageActivity.java
+++ PrimerAppMovil/app/src/main/java/com/example/user/primerappmovil/DisplayMessageActivity.java
@@ -1,7 +1,11 @@
 package com.example.user.primerappmovil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        Intent intent =getIntent();
+        String message =intent.getStringExtra(EXTRA_MESSAGE);
+        TextView textView =(TextView)findViewById(R.id.textView);
+        textView.setText(message);
     }
 }
