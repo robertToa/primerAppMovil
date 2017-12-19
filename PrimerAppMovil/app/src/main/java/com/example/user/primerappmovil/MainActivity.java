55f7c270d4269256ffc255063779051c2213a26e
DESKTOP-T42SNH4\USER <robrt_pm@hotmail.com>
2017-12-18 23:48:35 -0500

    Practica 04, se guarda la informacion en un String y se lo muestra en un taextView la infromacion guardada cuando se de clic en el boton "Last".

--- PrimerAppMovil/app/src/main/java/com/example/user/primerappmovil/MainActivity.java
+++ PrimerAppMovil/app/src/main/java/com/example/user/primerappmovil/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.user.primerappmovil;
 
+import android.content.Context;
 import android.content.Intent;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 import static com.example.user.primerappmovil.R.id.editText;
 
@@ -22,9 +26,23 @@ public class MainActivity extends AppCompatActivity {
         Intent intent =new Intent(this,DisplayMessageActivity.class);
         EditText editTex = (EditText)findViewById(editText);
         String message = editTex.getText().toString();
+
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.lastvalue), message);
+        editor.commit();
+
         intent.putExtra(EXTRA_MESSAGE,message);
         startActivity(intent);
     }
+
+    public void lastValue(View view) {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String lastValue = sharedPref.getString(getString(R.string.lastvalue),"None");
+        TextView textView= (TextView) findViewById(R.id.textView);
+        textView.setText(lastValue);
+    }
+
 
     public void VentanaImagen(View view){
         Intent intent =new Intent(this,DisplayImageActivity.class);
